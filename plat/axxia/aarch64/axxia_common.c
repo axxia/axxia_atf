5ad323b72115deeeaea2c7fd89bcbf3689a59bf8
John Jacques <john.jacques@lsi.com>
2015-02-13 10:59:41 -0600

    plat/axxia: Remove Multiple Mappings for BL31
    bl31 was mapped multiple times; overlapping.  This
    works in simulation, but not in emulation.
    
    Signed-off-by: John Jacques <john.jacques@lsi.com>

--- plat/axxia/aarch64/axxia_common.c
+++ plat/axxia/aarch64/axxia_common.c
@@ -44,7 +44,6 @@
  * configure_mmu_elx() will give the available subset of that,
  */
 static const mmap_region_t axxia_mmap[] = {
-	{ TZROM_BASE,		TZROM_BASE,		TZROM_SIZE,		MT_MEMORY | MT_RO | MT_SECURE },
 	{ DEVICE0_BASE,		DEVICE0_BASE,		DEVICE0_SIZE,		MT_DEVICE | MT_RW | MT_SECURE },
 	{ DEVICE1_BASE,		DEVICE1_BASE,		DEVICE1_SIZE,		MT_DEVICE | MT_RW | MT_SECURE },
 	{ DRAM_BASE,		DRAM_BASE,		DRAM_SIZE,		MT_MEMORY | MT_RW | MT_SECURE },
@@ -55,27 +54,24 @@ static const mmap_region_t axxia_mmap[] = {
  * Macro generating the code for the function setting up the pagetables as per
  * the platform memory map & initialize the mmu, for the given exception level
  ******************************************************************************/
-#define DEFINE_CONFIGURE_MMU_EL(_el)				\
-	void configure_mmu_el##_el(unsigned long total_base,	\
-				  unsigned long total_size,	\
-				  unsigned long ro_start,	\
-				  unsigned long ro_limit,	\
-				  unsigned long coh_start,	\
-				  unsigned long coh_limit)	\
-	{							\
-	       mmap_add_region(total_base, total_base,		\
-			       total_size,			\
-			       MT_MEMORY | MT_RW | MT_SECURE);	\
-	       mmap_add_region(ro_start, ro_start,		\
-			       ro_limit - ro_start,		\
-			       MT_MEMORY | MT_RO | MT_SECURE);	\
-	       mmap_add_region(coh_start, coh_start,		\
-			       coh_limit - coh_start,		\
-			       MT_DEVICE | MT_RW | MT_SECURE);	\
-	       mmap_add(axxia_mmap);				\
-	       init_xlat_tables();				\
-								\
-	       enable_mmu_el##_el(0);				\
+#define DEFINE_CONFIGURE_MMU_EL(_el)				   \
+	void configure_mmu_el##_el(unsigned long total_base,	   \
+				  unsigned long total_size,	   \
+				  unsigned long ro_start,	   \
+				  unsigned long ro_limit,	   \
+				  unsigned long coh_start,	   \
+				  unsigned long coh_limit)	   \
+	{							   \
+	       mmap_add_region(ro_start, ro_start,		   \
+			       ro_limit - ro_start,		   \
+			       MT_MEMORY | MT_RO | MT_SECURE);	   \
+	       mmap_add_region(ro_limit, ro_limit,                 \
+			       TZRAM_SIZE - (ro_limit - ro_start), \
+			       MT_MEMORY | MT_RW | MT_SECURE);	   \
+	       mmap_add(axxia_mmap);				   \
+	       init_xlat_tables();				   \
+								   \
+	       enable_mmu_el##_el(0);				   \
 	}
 
 /* Define EL1 and EL3 variants of the function initialising the MMU */
