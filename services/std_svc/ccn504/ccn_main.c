7ce0b31a964fd9dd566ff128b5705f4b3f55fd8b
Marek Majtyka <marekx.majtyka@intel.com>
2017-10-25 05:25:40 -0500

    Added edac l3 support for xlf.
    Dickens (ccn504) and Shally (ccn512) have different ccn base address.
    This commit fixes this so that now it distinguishes between those two
    in runtime for ccn SMC service.
    
    NOTE: This update is a must only for 6700. It does not affect 5600.
    
    Signed-off-by: Marek Majtyka <marekx.majtyka@intel.com>

--- services/std_svc/ccn504/ccn_main.c
+++ services/std_svc/ccn504/ccn_main.c
@@ -10,18 +10,20 @@
 
 #include "ccn_private.h"
 
+extern unsigned long dickens_base;
+
 #ifdef CCN_DEBUG
 int ccn_make_unsecure(uint64_t x1)
 {
 	uint64_t ret=1;
-	mmio_write_64(0x8000000000, 0x1);
+	mmio_write_64(dickens_base, 0x1);
 	return  ret;
 }
 
 int ccn_make_secure(uint64_t x1)
 {
 	uint64_t ret=1;
-	mmio_write_64(0x8000000000, 0x0);
+	mmio_write_64(dickens_base, 0x0);
 	return  ret;
 }
 #endif
@@ -36,7 +38,7 @@ uint64_t write_ccn_nm_errint_status(uint64_t x1)
 	case CCN_MN_ERRINT_STATUS__CORRECTED_ERRORS_DISABLE:
 	case CCN_MN_ERRINT_STATUS__PMU_EVENTS__ENABLE:
 	case CCN_MN_ERRINT_STATUS__PMU_EVENTS__DISABLE:
-		mmio_write_64(0x8000000008, x1);
+		mmio_write_64(dickens_base + 8, x1);
 		return 1;
 	default:
 
@@ -47,7 +49,7 @@ uint64_t write_ccn_nm_errint_status(uint64_t x1)
 
 uint64_t read_ccn_nm_errint_status(void)
 {
-	return mmio_read_64(0x8000000008);
+	return mmio_read_64(dickens_base + 8);
 }
 
 uint64_t ccn_smc_handler(uint32_t smc_fid,
@@ -86,7 +88,7 @@ uint64_t ccn_smc_handler(uint32_t smc_fid,
 			SMC_RET1(handle, ccn_make_unsecure(x1));
 #endif
 		case CCN_MN_READ_ERRINT_AARCH64:
-			SMC_RET1(handle, mmio_read_64(0x8000000008));
+			SMC_RET1(handle, mmio_read_64(dickens_base + 8));
 
 		case CCN_MN_WRITE_ERRINT_AARCH64:
 			SMC_RET1(handle, write_ccn_nm_errint_status(x1));
--- services/std_svc/ccn504/ccn_setup.c
+++ services/std_svc/ccn504/ccn_setup.c
@@ -1,10 +1,25 @@
 #include <bl_common.h>
 #include <stddef.h>
+#include <axxia_def.h>
+#include <axxia_private.h>
+#include <debug.h>
 #include "ccn_private.h"
 
+
 unsigned int ccn_caps;
+unsigned long dickens_base;
+
+
 
 void ccn504_setup(void)
 {
 	ccn_caps = CCN_GENERIC_CAP;
+
+	if (IS_5600())
+		dickens_base = DICKENS_BASE_X9;
+	else
+		dickens_base = DICKENS_BASE_XLF;
+#ifdef CCN_DEBUG
+	INFO("DICKENS: 0x%lx\n", dickens_base);
+#endif
 }
